c54bf102a90853d7b67d4ed2338c1136f22484e5
pam <jparado_25@yahoo.com>
2019-03-21 13:51:45 +0800

    base template and ERD

---
+++ resources/views/base.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
+
+        <meta name="author" content="UST Students"/>
+        <meta name="description" content="The E-Manager is a simple web appliction to create, manage, and report upon events sponsored by various
+        organisations. Events are posted for user perusal and feedback."/>
+    </head>
+
+    <title>E-Manager @yield('title')</title>
+</html>
