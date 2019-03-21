4af545115c9969de0b0e28e0317b59a4ff8fb85c
pam <jparado_25@yahoo.com>
2019-03-21 14:34:31 +0800

    Admin - Event relationship

--- app/Admin.php
+++ app/Admin.php
@@ -6,12 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 // Import Models
 use App\User;
+use App\Event;
 
 class Admin extends Model
 {
     //
-    public function User()
+    public function user()
     {
         return $this->hasOne(User::class);
+    }
+
+    public function events()
+    {
+        return $this->hasMany(Event::class);
     }
 }
