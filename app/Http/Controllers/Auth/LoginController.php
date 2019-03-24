d9613f4c178b71593776280191d24c3f9bb1014d
pam <jparado_25@yahoo.com>
2019-03-24 12:45:48 +0800

    Alternative redirect depending on user type

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -36,4 +36,14 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    public function authenticated()
+    {
+        $user = Auth::user();
+
+        if($user->type == 'ADMIN')
+        {
+            return redirect()->route('admin');
+        }
+    }
 }
--- app/Http/Controllers/SpeakerController.php
+++ app/Http/Controllers/SpeakerController.php
@@ -4,13 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class AdminController extends Controller
+class SpeakerController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
     /**
      * Display a listing of the resource.
      *
