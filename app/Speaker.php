b4d31bbb7c4a26af212129b423f8b32fc92dc889
pam <jparado_25@yahoo.com>
2019-03-25 05:55:13 +0800

    Fixed model table rel names

--- app/Speaker.php
+++ app/Speaker.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Speaker extends Model
 {
     //
+    protected $table = 'speaker';
 }
--- app/Sponsor.php
+++ app/Sponsor.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Sponsor extends Model
 {
     //
+    protected $table = 'sponsor';
 }
