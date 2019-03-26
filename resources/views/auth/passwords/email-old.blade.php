d0d250cc1037c76d6bbc3d0e63fd2af60fdfe781
pam <jparado_25@yahoo.com>
2019-03-27 05:34:23 +0800

    Edited the password reset link page
    App names now have a single source of truth

--- resources/views/auth/passwords/email-old.blade.php
+++ resources/views/auth/passwords/email-old.blade.php
@@ -1,11 +1,8 @@
-@extends('base')
-
-@section('title')
-    Forgot Password
-@endsection
+@extends('layouts.app')
 
 @section('content')
-    <div class="row justify-content-center mt-5">
+<div class="container">
+    <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Reset Password') }}</div>
@@ -46,4 +43,5 @@
             </div>
         </div>
     </div>
+</div>
 @endsection
